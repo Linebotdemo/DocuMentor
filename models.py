eb67099e8f7cfd089fe988a92fd97500c2e917ca
Linebotdemo <k.inose0902@gmail.com>
2025-04-12 10:03:10 +0900

    Fix whisper JSON header

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class Video(db.Model):
     filename = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quiz_text = db.Column(db.Text)
 
     # リレーション（Userモデルがある場合）
     user = db.relationship('User', backref=db.backref('videos', lazy=True))
